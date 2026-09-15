8e508a97b75616b6c9a6e631e027ce7b4327ab19
SHYAM69-C <shyamsah98121@gmail.com>
2026-09-15 22:32:38 +0530

    Time: 93 ms (3.32%), Space: 43.9 MB (83.96%) - LeetHub

---
+++ 0994-rotting-oranges/0994-rotting-oranges.java
@@ -0,0 +1,46 @@
+class Solution {
+    public int orangesRotting(int[][] grid) {
+         int m = grid.length;
+        int n = grid[0].length;
+        Queue<int[]> q = new LinkedList<>();
+        int fresh = 0;
+        
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (grid[i][j] == 2) {
+                    q.offer(new int[]{i, j});
+                } else if (grid[i][j] == 1) {
+                    fresh++;
+                }
+            }
+        }
+        
+        int minutes = 0;
+        int[][] dir = {{1,0}, {-1,0}, {0,1}, {0,-1}};
+        
+        while (!q.isEmpty() && fresh > 0) {
+            int size = q.size();
+            minutes++;
+            
+            for (int i = 0; i < size; i++) {
+                int[] curr = q.poll();
+                int r = curr[0];
+                int c = curr[1];
+                
+                for (int[] d : dir) {
+                    int nr = r + d[0];
+                    int nc = c + d[1];
+                    
+                    if (nr >= 0 && nr < m && nc >= 0 && nc < n 
+                        && grid[nr][nc] == 1) {
+                        grid[nr][nc] = 2;
+                        fresh--;
+                        q.offer(new int[]{nr, nc});
+                    }
+                }
+            }
+        }
+        
+        return fresh == 0 ? minutes : -1;
+    }
+}
